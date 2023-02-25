d6cc59f4477991900cd61ba27ef9b8974d6a4f05
Hoon Kim <eloael@outlook.com>
2023-02-24 21:50:29 -0800

    added some logic to app and index

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+// import Header from "./components/Header"; 
+import Page from "./components/Page"; /* Includes Components: About, Portfolio, Contact, & Resume */
+import DrawerAppBar from "./components/DrawerAppBar"; /* Includes Component: Navigation */
+
+function App() {
+  const [pages] = useState([
+    { name: "About Me" },
+    { name: "Portfolio" },
+    { name: "Contact" },
+    { name: "Resume" },
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+  const [pageSelected, setPageSelected] = useState(false);
+
+  return (
+    <div>
+      <DrawerAppBar
+        pages={pages}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        pageSelected={pageSelected}
+        setPageSelected={setPageSelected}
+      />
+      <section className="hero">
+        <div className="hero-name">
+          <p>Hoon Kim</p>
+          <div className="hero-text">
+            <p>Developer Stuff</p>
+          </div>
+        </div>
+      </section>
+      <main>
+        <Page currentPage={currentPage} />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
